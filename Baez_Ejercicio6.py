6a6d0e3e11b031561c0174be4d385b955747896f
Sebastian Baez <osebaez@gmail.com>
2023-11-29 16:07:48 -0300

    Agrego ejercicios Unidad 1

---
+++ Baez_Ejercicio6.py
@@ -0,0 +1,9 @@
+#Ejercicio 6.1
+print("El área de un triángulo cuya base es 10cm y altura 20cm es:", (10*20)//2)
+#Ejercicio 6.2
+print("El área de un cuadrado cuyos lados miden 40cm es:", 40*40)
+#Ejercicio 6.3
+print("El Perímetro de un triángulo equilátero de 9cm lado es:", 9*3)
+#Ejercicio 6.4
+print("El perímetro de un rectángulo de lado 10cm y lado 6cm es:", 2*(10+6))
+
---
+++ Baez_Ejercicios4y5.py
@@ -0,0 +1,7 @@
+import sys
+
+#Ejericio 4:
+#print("Hola", sys.argv[0])
+
+#Ejericio 5:
+print(sys.argv)
